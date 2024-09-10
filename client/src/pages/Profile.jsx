50ee4006efba97c63bcdbcea1a11d25c3870fddf
charan <sanamcharan6@gmail.com>
2024-09-10 19:49:28 +0530

    add delete user func

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 import { storage } from "../firebase";
 import { getDownloadURL, getStorage,ref, uploadBytesResumable } from "firebase/storage";
 import { useDispatch, useSelector } from "react-redux"
-import { updateUserStart,updateUserSuccess,updateUserFailure, signOutUserStart, signOutUserFailure, signOutUserSuccess } from "../redux/user/userSlice";
+import { updateUserStart,updateUserSuccess,updateUserFailure, signOutUserStart, signOutUserFailure, signOutUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/user/userSlice";
 import { app } from "../firebase";
 export default function Profile() {
   const fileRef= useRef(null);
@@ -91,6 +91,23 @@ export default function Profile() {
   }
 
 
+  const handleDeleteUser= async()=>{
+
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE',
+      });
+      const data = await res.json();
+    if(data.success===false){
+      dispatch(deleteUserFailure(data.message));
+      return;
+    }
+    dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -123,7 +140,7 @@ export default function Profile() {
       </form>
       
       <div className="flex justify-between mt-5">
-        <span className="text-red-400 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteUser} className="text-red-400 cursor-pointer">Delete Account</span>
         <span onClick={handleSignOut}  className="text-red-400 cursor-pointer">Sign out</span>
       </div>
 
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -46,9 +46,32 @@ const userSlice =createSlice({
             state.error = action.payload;
             state.loading=false;
         },
+        deleteUserStart: (state) => {
+            state.loading = true;
+          },
+          deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+          },
+          deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+          },
     },
 });
 
-export const { signInStart,signInSuccess,signInFailure,updateUserStart,updateUserSuccess,updateUserFailure,signOutUserStart,signOutUserSuccess,signOutUserFailure} = userSlice.actions;
+export const { signInStart,
+    signInSuccess,
+    signInFailure,
+    updateUserStart,
+    updateUserSuccess,
+    updateUserFailure,
+    signOutUserStart,
+    signOutUserSuccess,
+    signOutUserFailure,
+    deleteUserFailure,
+    deleteUserSuccess,
+    deleteUserStart,} = userSlice.actions;
 
 export default userSlice.reducer;
